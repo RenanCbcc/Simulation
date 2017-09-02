c36e31a2940906323bac00d1f3040087076e52c1
Renan Thiago <renannojosa@gmail.com>
2017-09-02 20:07:56 -0300

    Finalização da simulação

--- Bus.py
+++ Bus.py
@@ -5,22 +5,24 @@ import matplotlib.pyplot as plt
 class Simulation:
     def __init__(self):
         self.__tempo_percurso = []
+        self.__tempo_percurso_medio = []
 
     def perform(self,rodadas,replicacao,paradas):
-        for i in range(1,rodadas+1):
-            print("Rodada #%i" %i)
-            for j in range(1,replicacao+1):
+        for i in range(1, rodadas + 1):
+            print("Rodada #%i" % i)
+            for j in range(1, replicacao + 1):
                 print("Replicacao #%i" % j)
-                tempo_volta = 1500  # tempo médio que o ônibus completa uma volta (25min), desconsiderando tempo de parada.
-                for n in range(1,paradas+1):#para cada parada DO
-                    print("Parada #%i"%n)
-                    pessoas = np.random.randint(0,10)  # n pessoas (n>=0) subirão no ônibus.
+                tempo_percuso = 1560  # tempo médio que o ônibus completa uma volta (26min), desconsiderando tempo de parada.
+                for n in range(1, paradas + 1):  # para cada parada DO
+                    print("Parada #%i" % n)
+                    pessoas = np.random.poisson(4)  # n pessoas (n>=0) subirão no ônibus.
                     print(pessoas)
-                    tempo_volta += int(sum(np.random.normal(3, 1, pessoas)))# cada pessoas terá um tempo aleatório de embarque.
-                    print(tempo_volta/60)
-                #apos terminar o percurso, armazenar o tempo de parada + tempo do percurso
-                print(tempo_volta)
-                self.__tempo_percurso.append(tempo_volta)
+                    tempo_parada = int(
+                        sum(np.random.normal(4, 1, pessoas)))  # cada pessoas terá um tempo aleatório de embarque.
+                    tempo_percuso += tempo_parada
+                    print("%.2f" % (tempo_percuso / 60))
+                # apos terminar o percurso, armazenar o somatório dos tempos
+                self.__tempo_percurso.append(tempo_percuso)
 
         tempos = list(np.where( np.asarray(self.__tempo_percurso) > 1800, 1, -1))
         print("Média  de tempo de percurso do Circular: %i" %((sum(self.__tempo_percurso)/len(self.__tempo_percurso))/60))
@@ -35,10 +37,44 @@ class Simulation:
         plt.ylabel("Tempo em segundos")
         plt.show()
 
+    def simulate(self,rodadas,replicacao,paradas):
+        for i in range(1,rodadas+1):
+            print("Rodada #%i" %i)
+            for j in range(1,replicacao+1):
+                print("Replicacao #%i" % j)
+                tempo_percuso = 1560  # tempo médio que o ônibus completa uma volta (26min), desconsiderando tempo de parada.
+                for n in range(1,paradas+1):#para cada parada DO
+                    print("Parada #%i"%n)
+                    pessoas = np.random.poisson(4)  # n pessoas (n>=0) subirão no ônibus.
+                    print(pessoas)
+                    tempo_parada = int(sum(np.random.normal(4, 1, pessoas)))# cada pessoas terá um tempo aleatório de embarque.
+                    tempo_percuso += tempo_parada
+                    print("%.2f"%(tempo_percuso/60))
+                #apos terminar o percurso, armazenar o somatório dos tempos
+                self.__tempo_percurso.append(tempo_percuso)
+            #Como no slide, armazenar a média das replicações
+            self.__tempo_percurso_medio.append(sum(self.__tempo_percurso)/len(self.__tempo_percurso))
+            self.__tempo_percurso.clear()
+
+        self.__tempo_percurso_medio.pop(0)
+        tempos = list(np.where( np.asarray(self.__tempo_percurso_medio) > 1800, 1, -1))
+        print("Média  dos tempos de percurso do Circular: %i" %((sum(self.__tempo_percurso_medio)/len(self.__tempo_percurso_medio))/60))
+        print("Numero de vezes que o tempo excedeu 30 min: %f"%tempos.count(1))
+        print("Probabilidade de uma volta ter mais de 30min: %f" % (tempos.count(1) / len(tempos)))
+        for i in range(len(self.__tempo_percurso_medio)):
+            self.__tempo_percurso_medio[i] = self.__tempo_percurso_medio[i]/60
+
+        plt.plot(range(len(self.__tempo_percurso_medio)),self.__tempo_percurso_medio, linestyle='--', color='g', marker='s',linewidth=1.0)
+        plt.title("Simulação do Circular da UFPA (Media de replicações)")
+        plt.xlabel("Replicações")
+        plt.ylabel("Tempo em segundos")
+        plt.show()
+
+
 
 ufpa = Simulation()
-ufpa.perform(5,24,10)#(5 dias na semana, 24 pois são doze horas de funcinamento e são 2 voltas por hora, arbitrario)
-
+ufpa.perform(20,24,15)#(5 dias na semana, 24 pois são doze horas de funcinamento e são 2 voltas por hora, arbitrario)
+ufpa.simulate(20,24,15)
 
 
 
