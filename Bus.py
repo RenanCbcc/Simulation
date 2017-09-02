41492f680e18535059d4d09689e0ff559b499ebb
Renan Thiago <renannojosa@gmail.com>
2017-09-02 17:31:59 -0300

    Trabalho final

--- Bus.py
+++ Bus.py
@@ -7,15 +7,14 @@ class Simulation:
         self.__tempo_percurso = []
 
     def perform(self,rodadas,replicacao,paradas):
-        self.__tempo_percurso.clear()
         for i in range(1,rodadas+1):
             print("Rodada #%i" %i)
             for j in range(1,replicacao+1):
                 print("Replicacao #%i" % j)
-                tempo_volta = 1500  # tempo médio que o ônibus completa uma volta, desconsiderando tempo de parada.
+                tempo_volta = 1500  # tempo médio que o ônibus completa uma volta (25min), desconsiderando tempo de parada.
                 for n in range(1,paradas+1):#para cada parada DO
                     print("Parada #%i"%n)
-                    pessoas = np.random.randint(0,20)  # n pessoas (n>=0) subirão no ônibus.
+                    pessoas = np.random.randint(0,10)  # n pessoas (n>=0) subirão no ônibus.
                     print(pessoas)
                     tempo_volta += int(sum(np.random.normal(3, 1, pessoas)))# cada pessoas terá um tempo aleatório de embarque.
                     print(tempo_volta/60)
@@ -24,10 +23,17 @@ class Simulation:
                 self.__tempo_percurso.append(tempo_volta)
 
         tempos = list(np.where( np.asarray(self.__tempo_percurso) > 1800, 1, -1))
+        print("Média  de tempo de percurso do Circular: %i" %((sum(self.__tempo_percurso)/len(self.__tempo_percurso))/60))
         print("Numero de vezes que o tempo excedeu 30 min: %f"%tempos.count(1))
         print("Probabilidade de uma volta ter mais de 30min: %f" % (tempos.count(1) / len(tempos)))
-        plt.plot(tempos)
+        for i in range(len(self.__tempo_percurso)):
+            self.__tempo_percurso[i] = self.__tempo_percurso[i]/60
 
+        plt.plot(range(len(self.__tempo_percurso)),self.__tempo_percurso, linestyle='--', color='g', marker='s',linewidth=1.0)
+        plt.title("Simulação do Circular da UFPA")
+        plt.xlabel("Replicações")
+        plt.ylabel("Tempo em segundos")
+        plt.show()
 
 
 ufpa = Simulation()
